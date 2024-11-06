c0ade6a9d077b4df499bd75ff3c1fabe4e94f304
Samrath <samrathreddy04@gmail.com>
2024-11-07 00:26:00 +0530

    Vercel try..!!

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ const feeRoutes = require('./Routes/feeRoute');
 const paymentRoutes = require('./Routes/paymentRoute');
 
 const app = express();
-const PORT = process.env.PORT || 8000;
 
 // CORS configuration
 const corsOptions = {
@@ -19,16 +18,16 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 
-// Handle preflight requests
-app.options('*', cors(corsOptions)); // Preflight requests are always OPTIONS
-
 // Apply CORS and JSON body parser middleware
+app.options('*', cors(corsOptions)); // Preflight requests
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(limiter);
 
+// Connect to the database
 connectDB();
 
+// Define routes
 app.use('/v1/api/roll', rollRoutes);
 app.use('/v1/api/dob', dobRoutes);
 app.use('/v1/api/fee', feeRoutes);
@@ -37,6 +36,5 @@ app.use("/v1/api/razor/getkey", (req, res) =>
   res.status(200).json({ key: process.env.RAZORPAY_API_KEY })
 );
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Export the app for Vercel to use as a serverless function
+module.exports = app;
