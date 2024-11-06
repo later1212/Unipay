a1c4736ac09ac39c7bcfe079e32bf9b17298cabe
Samrath <samrathreddy04@gmail.com>
2024-11-07 01:49:21 +0530

    tryyee!!

--- server/config/db.js
+++ server/config/db.js
@@ -3,13 +3,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
-    console.log("DB is running");
-
     // Retrieve and print all collections
     const db = mongoose.connection.db;
     const collections = await db.listCollections().toArray();
-    console.log("Collections:");
-    collections.forEach(collection => console.log(collection.name));
   } catch (err) {
     console.error(err);
   }
